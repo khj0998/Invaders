f439ea36229e601e37161562278c03bc8114b291
JUN YOUNG <jyahn2008@naver.com>
2023-12-09 11:27:25 +0900

    테스트 코드 추가

--- Test/entity/BossTest.java
+++ Test/entity/BossTest.java
@@ -5,6 +5,7 @@ import engine.GameState;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,6 +16,7 @@ class BossTest {
     @BeforeEach
     void setUp() {
         boss = new Boss(0,0, DrawManager.SpriteType.Boss,new GameState(1,1,1,1,1));
+        bullets = new HashSet<Bullet>();
     }
 
     @Test
@@ -27,13 +29,25 @@ class BossTest {
 
     @Test
     void bossShoot() {
+        boss.bossShoot(this.bullets,0,0,20);
+        boss.bossShoot(this.bullets,6,5,20);
+        boss.bossShoot(this.bullets,8,1,20);
+        boss.bossShoot(this.bullets,4,6,20);
+        for (Bullet bullet: bullets){
+            assertEquals(bullet.getSpeed(),20);
+        }
     }
 
     @Test
     void boosShootWithX() {
-    }
+        boss.boosShootWithX(this.bullets,0,50,20,10);
+        boss.boosShootWithX(this.bullets,0,410,20,10);
+        boss.boosShootWithX(this.bullets,0,00,20,10);
+        boss.boosShootWithX(this.bullets,0,100,20,10);
 
-    @Test
-    void bossAttacked() {
+        for (Bullet bullet: bullets){
+            assertEquals(bullet.getSpeed(),10);
+            assertEquals(bullet.getX_speed(),20);
+        }
     }
 }
